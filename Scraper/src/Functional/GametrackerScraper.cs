2600dbb88e1b011df7144fa559d5f4a21b11740d
TortelliniSRL <simonetenisci10@gmail.com>
2025-01-27 23:09:11 +0100

    [feat] GametrackerScraper: Players list extraction

--- Scraper/src/Functional/GametrackerScraper.cs
+++ Scraper/src/Functional/GametrackerScraper.cs
@@ -1,6 +1,7 @@
 using OpenQA.Selenium;
 using GametrackerServerScraper.Models;
 using System.Net;
+using System.Collections.ObjectModel;
 
 namespace GametrackerServerScraper.Functional;
 
@@ -23,12 +24,37 @@ public class GametrackerScraper : IScraper
         IWebElement currentPlayersElement = driver.FindElement(By.Id("HTML_num_players"));
         IWebElement maxPlayersElement = driver.FindElement(By.Id("HTML_max_players"));
 
+        List<string> players = [];
+        try 
+        {
+            IEnumerable<IWebElement> playersListElement = driver.FindElement(By.Id("HTML_online_players"))
+                .FindElement(By.ClassName("table_lst_stp"))
+                .FindElement(By.TagName("tbody"))
+                .FindElements(By.TagName("tr")).Skip(1)
+            ;
+
+            foreach (IWebElement playerElement in playersListElement)
+            {
+                ReadOnlyCollection<IWebElement> cells = playerElement.FindElements(By.TagName("td"));
+
+                string name = cells[1].FindElement(By.TagName("a")).Text.Trim();
+
+                players.Add(name);
+            }
+        }
+        catch (NoSuchElementException) 
+        {
+            // No players online
+        }
+        
+
         ServerInfo info = new ServerInfo
         {
             Name = nameElement.Text,
             CurrentMap = currentMapElement.Text,
             CurrentPlayers = int.Parse(currentPlayersElement.Text ?? "0"),
-            MaxPlayers = int.Parse(maxPlayersElement.Text ?? "0")
+            MaxPlayers = int.Parse(maxPlayersElement.Text ?? "0"),
+            Players = players
         };
 
         return info;
